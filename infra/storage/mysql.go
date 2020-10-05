06a76f48ceceabe4bb504a6b2321961a6fac5cd2
Tony C. Batista <tcbatista@uolinc.com>
2020-10-05 10:32:48 -0300

    connection database with retry

--- infra/storage/mysql.go
+++ infra/storage/mysql.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql" // Register MySQL operations
 	"github.com/pkg/errors"
@@ -10,14 +11,35 @@ import (
 
 // NewMySQLConnection creates a new mysql connection
 func NewMySQLConnection(c Config) (*sql.DB, error) {
-	db, err := sql.Open(c.host, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", c.user, c.password, c.host, c.port, c.database))
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to connect to mysql database")
+	toRetry := func() (*sql.DB, error) {
+		db, err := sql.Open(c.host, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", c.user, c.password, c.host, c.port, c.database))
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to connect to mysql database")
+		}
+
+		if err = db.Ping(); err != nil {
+			return nil, errors.Wrap(err, "database unavailable")
+		}
+
+		return db, nil
 	}
 
-	if err = db.Ping(); err != nil {
-		return nil, errors.Wrap(err, "database unavailable")
-	}
+	return retry(toRetry, 20)
+}
 
-	return db, nil
+func retry(fn func() (*sql.DB, error), maxRetries int) (*sql.DB, error) {
+	i := 0
+	for {
+		conn, err := fn()
+		if err == nil {
+			return conn, nil
+		}
+
+		if i >= maxRetries {
+			return nil, errors.Wrap(err, fmt.Sprintf("error after %d attemps", i))
+		}
+
+		i++
+		time.Sleep(1 * time.Second)
+	}
 }
